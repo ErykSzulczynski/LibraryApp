1102cb949246213abe49ff7a63da3621d008fe1d
Eryk Szułczyński <eryk.szulczynski0@gmail.com>
2019-07-20 21:03:22 +0200

    Main page basic layout and styling

--- index.php
+++ index.php
@@ -2,8 +2,67 @@
     require_once('./view/header.php');
 ?>
 <body>
-<?php
-    require_once('./view/sidebar.php');
-?>
+<div class="container">
+    <div class="sidebar__wrapper pt-4">
+        <?php
+            require_once('./view/sidebar.php');
+        ?>
+    </div>
+    <div class="content__wrapper p-5">
+        <div class="row1 m-5">
+            <div class="card bg-light mb-3" style="min-width: 55rem;">
+                <div class="card-header">Wypożyczenia</div>
+                <div class="card-body">
+                    <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="row2 my-5">
+            <div class="card bg-light mb-5" style="min-width: 20rem;">
+                <div class="card-header">Powiadomienia</div>
+                <div class="card-body">
+                <h5 class="card-title">Light card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card bg-light" style="min-width: 20rem;">
+                <div class="card-header">Rezerwacje</div>
+                <div class="card-body">
+                <h5 class="card-title">Light card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 </html>
--- view/sidebar.php
+++ view/sidebar.php
@@ -1,46 +1,5 @@
-<div class="sidebar">
-  <nav class="sidebar-nav">
-    <ul class="nav">
-      <li class="nav-title">Nav Title</li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="nav-icon cui-speedometer"></i> Nav item
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-          <i class="nav-icon cui-speedometer"></i> With badge
-          <span class="badge badge-primary">NEW</span>
-        </a>
-      </li>
-      <li class="nav-item nav-dropdown">
-        <a class="nav-link nav-dropdown-toggle" href="#">
-          <i class="nav-icon cui-puzzle"></i> Nav dropdown
-        </a>
-        <ul class="nav-dropdown-items">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <i class="nav-icon cui-puzzle"></i> Nav dropdown item
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <i class="nav-icon cui-puzzle"></i> Nav dropdown item
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="nav-item mt-auto">
-        <a class="nav-link nav-link-success" href="https://coreui.io">
-          <i class="nav-icon cui-cloud-download"></i> Download CoreUI</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link nav-link-danger" href="https://coreui.io/pro/">
-          <i class="nav-icon cui-layers"></i> Try CoreUI
-          <strong>PRO</strong>
-        </a>
-      </li>
-    </ul>
-  </nav>
-  <button class="sidebar-minimizer brand-minimizer" type="button"></button>
-</div>
+<nav class="nav flex-column">
+  <a class="nav-link" href="#"><button class = "btn btn-light text-primary font-weight-light" style = "font-size: 1.25rem"><i class="fas fa-tachometer-alt pr-3"></i>Dashboard</button></a>
+  <a class="nav-link" href="#"><button class = "btn btn-light text-primary font-weight-light" style = "font-size: 1.25rem"><i class="fas fa-book pr-3"></i>Books</button></a>
+  <a class="nav-link" href="#"><button class = "btn btn-light text-primary font-weight-light" style = "font-size: 1.25rem"><i class="fas fa-envelope pr-3"></i>Contact</button></a>
+</nav>
